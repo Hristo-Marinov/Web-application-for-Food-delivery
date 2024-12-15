d37aa93cdc72ca5bd9e43193da4997287e7ebd12
Hristo-Marinov <hristomarinov110506@gmail.com>
2024-12-15 20:32:19 +0200

    First Database
    Starting Database

---
+++ FoodEx/FoodEx/Entity/UserFavorite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodEx.Entity
+{
+    public class UserFavorite
+    {
+        [Key]
+        public int FavoriteId { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+        public int RestaurantId { get; set; }
+        [ForeignKey("RestaurantId")]
+        public Restaurant Restaurant { get; set; }
+    }
+}
---
+++ FoodEx/FoodEx/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodEx.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
